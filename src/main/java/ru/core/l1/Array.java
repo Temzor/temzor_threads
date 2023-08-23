4f6422b26562d341ee92e667b54af1f5f3f1a03c
Капустин Дмитрий <onewaykd@gmail.com>
2023-08-23 15:21:21 +0300

    Core #9

---
+++ src/main/java/ru/core/l1/Array.java
@@ -0,0 +1,23 @@
+package ru.core.l1;
+
+public class Array {
+    public static void main(String[] args) {
+        int[] daysInMonth = new int[12];
+        for (int i = 0; i < daysInMonth.length; i++) {
+            if (i % 2 == 0) {
+                daysInMonth[i] = 31;
+            }
+            if (i == 1) {
+                daysInMonth[i] = 28;
+            }
+            if (i % 2 != 0 && i != 1) {
+                daysInMonth[i] = 30;
+            }
+        }
+
+        for (int i : daysInMonth) {
+            System.out.println(i);
+        }
+
+    }
+}
---
+++ src/main/java/ru/core/l1/ArrayFirst.java
@@ -0,0 +1,24 @@
+package ru.core.l1;
+
+public class ArrayFirst {
+    public static void main(String[] args) {
+        int[] firstArray = new int[900];
+        int[] secondArray = new int[firstArray.length];
+
+        for (int i = 0; i < firstArray.length; i++) {
+            firstArray[i] = i + 100;
+        }
+
+        for (int i = 0, j = firstArray.length - 1; i < firstArray.length; i++, j--) {
+            secondArray[j] = firstArray[i];
+        }
+
+        for (int i : firstArray) {
+            System.out.println(i);
+        }
+
+        for (int j : secondArray) {
+            System.out.println(j);
+        }
+    }
+}
