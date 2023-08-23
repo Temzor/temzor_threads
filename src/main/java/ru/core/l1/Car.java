3d9eeb6384bd19d55817b8b7a29d913224c80c59
Капустин Дмитрий <onewaykd@gmail.com>
2023-08-23 12:09:22 +0300

    Core #6

---
+++ src/main/java/ru/core/l1/Car.java
@@ -0,0 +1,4 @@
+package ru.core.l1;
+
+public class Car extends Transport {
+}
---
+++ src/main/java/ru/core/l1/HierarchyUsage.java
@@ -0,0 +1,16 @@
+package ru.core.l1;
+
+public class HierarchyUsage {
+    public static void main(String[] args) {
+        Car car = new Car();
+        Transport transport = car;
+        Object object = car;
+        Object ocar = new Car();
+        Car carFromObject = (Car) ocar;
+        Object bicycle = new Bicycle();
+
+        System.out.println(new Car());
+        System.out.println(new Bicycle());
+        System.out.println(new Object());
+    }
+}
