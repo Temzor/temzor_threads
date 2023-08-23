5db65f477379eb36e4f041d4b3c5cd79adeb70cc
Капустин Дмитрий <onewaykd@gmail.com>
2023-08-23 11:37:04 +0300

    Core #5

---
+++ src/main/java/ru/core/l1/Money.java
@@ -0,0 +1,14 @@
+package ru.core.l1;
+
+public class Money {
+    public static void main(String[] args) {
+        int money = 750;
+        if (money > 500) {
+            System.out.println("Buy pizza");
+        } else if (money < 100) {
+            System.out.println("Buy lunch");
+        } else {
+            System.out.println("Buy cola");
+        }
+    }
+}
---
+++ src/main/java/ru/core/l1/Temp.java
@@ -0,0 +1,14 @@
+package ru.core.l1;
+
+public class Temp {
+    public static void main(String[] args) {
+        int temp = 23;
+        if (temp > 25) {
+            System.out.println("Start conditions");
+        } else if (temp < 22) {
+            System.out.println("Not start conditions");
+        } else {
+            System.out.println("Conditions is busy");
+        }
+    }
+}
