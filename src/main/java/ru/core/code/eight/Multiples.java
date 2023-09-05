aa0c9c2b26755e0e514cc0620dea10bb0e817d62
Капустин Дмитрий <onewaykd@gmail.com>
2023-09-05 17:58:49 +0300

    8 kyu Find Multiples of a Number

---
+++ src/main/java/ru/core/code/eight/Multiples.java
@@ -0,0 +1,13 @@
+package ru.core.code.eight;
+
+public class Multiples {
+    public static int[] find(int base, int limit) {
+        int[] result = new int[limit / base];
+        int counter = base;
+        for (int i = 0; i < result.length; i++) {
+            result[i] = counter;
+            counter += base;
+        }
+        return result;
+    }
+}
---
+++ src/test/java/ru/core/code/eight/MultiplesTest.java
@@ -0,0 +1,13 @@
+package ru.core.code.eight;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class MultiplesTest {
+    @Test
+    public void whenFiveAdnTwentyFiveLimitThenResultCorrect() {
+        assertThat(new int[] {5, 10, 15, 20, 25}).isEqualTo(Multiples.find(5, 25));
+    }
+
+}
