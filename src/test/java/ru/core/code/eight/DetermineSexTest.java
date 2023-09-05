62ef2a71271977b9870e78f427fc450125f55480
Капустин Дмитрий <onewaykd@gmail.com>
2023-09-05 18:12:04 +0300

    8 kyu Determine offspring sex based on genes XX and XY chromosomes

---
+++ src/test/java/ru/core/code/eight/DetermineSexTest.java
@@ -0,0 +1,20 @@
+package ru.core.code.eight;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class DetermineSexTest {
+    @Test
+    public void whenXXThenSon() {
+        assertThat(DetermineSex.chromosomeCheck("XX"))
+                .isEqualTo("Congratulations! You're going to have a son.");
+    }
+
+    @Test
+    public void whenXYThenDaughter() {
+        assertThat(DetermineSex.chromosomeCheck("XY"))
+                .isEqualTo("Congratulations! You're going to have a daughter.");
+    }
+
+}
