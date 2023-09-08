3f7d10488ee3a8bb315c1f0b7230763ce34954ed
Капустин Дмитрий <onewaykd@gmail.com>
2023-09-08 20:34:26 +0300

    Core #36.

--- src/main/java/ru/core/l2/collections/arrayl/Car.java
+++ src/main/java/ru/core/l2/collections/arrayl/Car.java
@@ -1,5 +1,7 @@
 package ru.core.l2.collections.arrayl;
 
+import java.util.Objects;
+
 public class Car {
     private final String brand;
     private final int vin;
@@ -15,5 +17,22 @@ public class Car {
 
     public int getVin() {
         return vin;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Car car = (Car) o;
+        return vin == car.vin && Objects.equals(brand, car.brand);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(brand, vin);
     }
 }
