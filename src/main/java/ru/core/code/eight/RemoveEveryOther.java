920632bb32d9716c55127d355979555d2c0ac7aa
Капустин Дмитрий <onewaykd@gmail.com>
2023-09-10 00:27:54 +0300

    8 kyu; Removing Elements.

--- src/main/java/ru/core/code/eight/RemoveEveryOther.java
+++ src/main/java/ru/core/code/eight/RemoveEveryOther.java
@@ -2,15 +2,10 @@ package ru.core.code.eight;
 
 public class RemoveEveryOther {
     public static Object[] removeEveryOther(Object[] array) {
-        if (array.length == 0) {
-            return array;
-        }
-        Object[] result = new Object[array.length % 2 == 0 ? array.length / 2 : array.length / 2 + 1];
-        result[0] = array[0];
-        int j = 2;
-        for (int i = 1; i < result.length; i++) {
-            result[i] = array[j];
-            j += 2;
+        Object[] result = new Object[(array.length + 1) / 2];
+
+        for (int i = 0; i < result.length; i++) {
+            result[i] = array[i * 2];
         }
         return result;
     }
