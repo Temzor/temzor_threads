9942102de30b4c13449f216e6198a2d12df7179d
Капустин Дмитрий <onewaykd@gmail.com>
2023-09-10 00:32:12 +0300

    fix issue for Calculator class.

--- src/main/java/ru/core/l2/test/Calculator.java
+++ src/main/java/ru/core/l2/test/Calculator.java
@@ -1,23 +1,23 @@
 package ru.core.l2.test;
 
 public class Calculator {
-    public double concat(double a, double b) {
+    public static double concat(double a, double b) {
         return a + b;
     }
 
-    public double concat(String a, String b) {
+    public static double concat(String a, String b) {
         return Integer.parseInt(a) + Integer.parseInt(b);
     }
 
-    public double minus(double a, double b) {
+    public static double minus(double a, double b) {
         return a - b;
     }
 
-    public double multiply(double a, double b) {
+    public static double multiply(double a, double b) {
         return a * b;
     }
 
-    public double divided(double a, double b) {
+    public static double divided(double a, double b) {
         return a / b;
     }
 }
--- src/test/java/ru/core/l2/test/CalculatorTest.java
+++ src/test/java/ru/core/l2/test/CalculatorTest.java
@@ -2,70 +2,58 @@ package ru.core.l2.test;
 
 import static org.assertj.core.api.Assertions.*;
 
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 class CalculatorTest {
-    private Calculator calculator;
-
-    @BeforeAll
-    public void setUpCalculator() {
-        calculator = new Calculator();
-    }
 
     @Test
     void whenCalculatorThenConcatPlus() {
-        assertThat(calculator.concat(12, 42)).isEqualTo(54);
+        assertThat(Calculator.concat(12, 42)).isEqualTo(54);
     }
 
     @Test
     void whenCalculatorThenConcatMinus() {
-        assertThat(calculator.concat(-18, -17)).isEqualTo(-35);
+        assertThat(Calculator.concat(-18, -17)).isEqualTo(-35);
     }
 
     @Test
     void whenCalculatorThenConcatPlusString() {
-        assertThat(calculator.concat("12", "42")).isEqualTo(54);
+        assertThat(Calculator.concat("12", "42")).isEqualTo(54);
     }
 
     @Test
     void whenCalculatorThenConcatMinusString() {
-        assertThat(calculator.concat("-18", "-17")).isEqualTo(-35);
+        assertThat(Calculator.concat("-18", "-17")).isEqualTo(-35);
     }
 
     @Test
     void whenCalculatorThenMinusPlus() {
-        assertThat(calculator.minus(44, 12)).isEqualTo(32);
+        assertThat(Calculator.minus(44, 12)).isEqualTo(32);
     }
 
     @Test
     void whenCalculatorThenMinusMinus() {
-        assertThat(calculator.minus(-14, -28)).isEqualTo(14);
+        assertThat(Calculator.minus(-14, -28)).isEqualTo(14);
     }
 
     @Test
     void whenCalculatorThenMultiplyPlus() {
-        assertThat(calculator.multiply(24, 8)).isEqualTo(192);
+        assertThat(Calculator.multiply(24, 8)).isEqualTo(192);
     }
 
     @Test
     void whenCalculatorThenMultiplyMinus() {
-        assertThat(calculator.multiply(-35, 77)).isEqualTo(-2_695);
+        assertThat(Calculator.multiply(-35, 77)).isEqualTo(-2_695);
     }
 
     @Test
     void whenCalculatorThenDividedPlus() {
-        assertThat(calculator.divided(24, 8)).isEqualTo(3);
+        assertThat(Calculator.divided(24, 8)).isEqualTo(3);
     }
 
     @Test
     void whenCalculatorThenDividedMinus() {
-        assertThat(calculator.divided(-44, 4)).isEqualTo(-11);
+        assertThat(Calculator.divided(-44, 4)).isEqualTo(-11);
     }
 
-    @AfterAll
-    public void close() {
-        calculator = null;
-    }
 }
