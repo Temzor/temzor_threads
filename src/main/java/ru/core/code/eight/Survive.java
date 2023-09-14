845c778e45a6666ddcd5d38df856060b2f91eab5
Капустин Дмитрий <onewaykd@gmail.com>
2023-09-14 21:32:42 +0300

    8 kyu; Is he gonna survive.

---
+++ src/main/java/ru/core/code/eight/Survive.java
@@ -0,0 +1,7 @@
+package ru.core.code.eight;
+
+public class Survive {
+    public static boolean hero(int bullets, int dragons) {
+        return bullets / dragons >= 2;
+    }
+}
---
+++ src/test/java/ru/core/code/eight/SurviveTest.java
@@ -0,0 +1,22 @@
+package ru.core.code.eight;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class SurviveTest {
+    @Test @DisplayName("A true hero")
+    void aTrueHero() {
+        assertThat(Survive.hero(10, 5)).isTrue();
+        assertThat(Survive.hero(100, 40)).isTrue();
+        assertThat(Survive.hero(10, 4)).isTrue();
+    }
+
+    @Test @DisplayName("A false hero")
+    void aFalseHero() {
+        assertThat(Survive.hero(10, 6)).isFalse();
+        assertThat(Survive.hero(100, 400)).isFalse();
+        assertThat(Survive.hero(10, 18)).isFalse();
+    }
+}
