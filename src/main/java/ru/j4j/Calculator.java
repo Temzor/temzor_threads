5d1e85c5cc272a50790b4abd5f890f5f8d890622
Капустин Дмитрий <onewaykd@gmail.com>
2023-09-16 12:43:00 +0300

    https://job4j.ru/profile/exercise/6/task-view/1025

---
+++ src/main/java/ru/j4j/Calculator.java
@@ -0,0 +1,21 @@
+package ru.j4j;
+
+public class Calculator {
+
+    public static void plus(int first, int second) {
+        int result = first + second;
+        System.out.println(result);
+    }
+
+    public static void minus(int first, int second) {
+        int result = first - second;
+        System.out.println(result);
+    }
+
+    public static void main(String[] args) {
+        plus(1, 2);
+        plus(10, 11);
+        minus(9, 8);
+        minus(6, 18);
+    }
+}
