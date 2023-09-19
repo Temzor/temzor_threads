3f1df494bb4c9b80eb3758449284be5e916b6eee
d.kapustin <onewaykd@gmail.com>
2023-09-19 16:08:43 +0300

    4.3.5. Множественное логическое выражение И; https://job4j.ru/profile/exercise/13/task-view/63

---
+++ src/test/java/ru/j4j/condition/TriangleTest.java
@@ -0,0 +1,18 @@
+package ru.j4j.condition;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TriangleTest {
+    @Test
+    void testTrue() {
+        assertThat(Triangle.exist(12, 14, 15)).isTrue();
+    }
+
+    @Test
+    void testFalse() {
+        assertThat(Triangle.exist(1, 14, 15)).isFalse();
+    }
+
+}
