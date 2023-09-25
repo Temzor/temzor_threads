ac821506f68e82285a61fa3d7e96d9454d8b40a2
d.kapustin <onewaykd@gmail.com>
2023-09-25 14:40:49 +0300

    5.7. Ипотека; https://job4j.ru/profile/exercise/14/task-view/80

---
+++ src/test/java/ru/j4j/loop/Mortgage.java
@@ -0,0 +1,13 @@
+package ru.j4j.loop;
+
+public class Mortgage {
+    public static int year(double amount, int salary, double percent) {
+        int year = 0;
+        while (amount > 0) {
+            amount += amount * (percent / 100);
+            amount -= salary;
+            year++;
+        }
+        return year;
+    }
+}
