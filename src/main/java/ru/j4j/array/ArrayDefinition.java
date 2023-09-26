3a5e434cf666158fa846013ac43f1d2991083370
d.kapustin <onewaykd@gmail.com>
2023-09-26 14:39:55 +0300

    6.0.3. Заполнение массива; https://job4j.ru/profile/exercise/2/task-view/88

--- src/main/java/ru/j4j/array/ArrayDefinition.java
+++ src/main/java/ru/j4j/array/ArrayDefinition.java
@@ -5,9 +5,15 @@ public class ArrayDefinition {
         short[] ages = new short[10];
         String[] surnames = new String[100500];
         float[] prices = new float[40];
+        String[] names = new String[4];
 
         System.out.println("Length array ages is: " + ages.length);
         System.out.println("Length array surnames is: " + surnames.length);
         System.out.println("Length array prices is: " + prices.length);
+
+        for (int i = 0; i < names.length; i++) {
+            names[i] = "Ive " + i;
+            System.out.println(names[i]);
+        }
     }
 }
