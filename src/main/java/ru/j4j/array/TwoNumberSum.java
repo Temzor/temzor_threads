e8963a4cbbdb7d67e366323dc100a26a16a7222f
d.kapustin <onewaykd@gmail.com>
2023-10-05 13:39:55 +0300

    6.8. Метод двух указателей; Array traversal check, algorithm acceleration https://job4j.ru/profile/exercise/2/task-view/116

--- src/main/java/ru/j4j/array/TwoNumberSum.java
+++ src/main/java/ru/j4j/array/TwoNumberSum.java
@@ -6,14 +6,17 @@ public class TwoNumberSum {
         int j = array.length - 1;
         while (i < j) {
             int sum = array[i] + array[j];
+            if (i > array.length / 2 || j < array.length / 2) {
+                break;
+            }
             if (sum == target) {
-                return new int[]{i, j};
+                return new int[] {i, j};
             } else if (sum < target) {
                 i++;
             } else {
                 j--;
             }
         }
-        return new int[]{};
+        return new int[] {};
     }
 }
