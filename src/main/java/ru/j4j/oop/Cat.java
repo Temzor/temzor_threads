0a3f79ae15a4641e55ad991d5fa0ef98ce73933a
d.kapustin <onewaykd@gmail.com>
2023-10-20 09:44:30 +0300

    1.1. Создание объекта; https://job4j.ru/profile/exercise/20/task-view/133

---
+++ src/main/java/ru/j4j/oop/Cat.java
@@ -0,0 +1,8 @@
+package ru.j4j.oop;
+
+public class Cat {
+    public static void main(String[] args) {
+        Cat petty = new Cat();
+        Cat sparky = new Cat();
+    }
+}
---
+++ src/main/java/ru/j4j/oop/Dog.java
@@ -0,0 +1,9 @@
+package ru.j4j.oop;
+
+public class Dog {
+    public static void main(String[] args) {
+        Dog polkan = new Dog();
+        Dog sharik = new Dog();
+        Dog zuchka = new Dog();
+    }
+}
