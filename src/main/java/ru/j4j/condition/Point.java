9a137c14a1c188aa413bc832be066beb9c39037a
d.kapustin <onewaykd@gmail.com>
2023-10-30 16:36:06 +0300

    2.0. Рефакторинг - Расстояние между точками. https://job4j.ru/profile/exercise/20/task-view/141

--- src/main/java/ru/j4j/condition/Point.java
+++ src/main/java/ru/j4j/condition/Point.java
@@ -1,7 +1,15 @@
 package ru.j4j.condition;
 
 public class Point {
-    public static double distance(int x1, int y1, int x2, int y2) {
-        return Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+    private final int x;
+    private final int y;
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public double distance(Point that) {
+        return Math.sqrt(Math.pow(this.x - that.x, 2) + Math.pow(this.y - that.y, 2));
     }
 }
--- src/test/java/ru/j4j/condition/PointTest.java
+++ src/test/java/ru/j4j/condition/PointTest.java
@@ -7,13 +7,21 @@ import org.junit.jupiter.api.Test;
 class PointTest {
     @Test
     public void testPositiveData() {
-        assertThat(Point.distance(12, 19, 23, 42))
-                .isCloseTo(25.4, within(0.1));
+        Point first = new Point(12, 19);
+        Point second = new Point(23, 42);
+        double result = 25.5;
+        double expect = first.distance(second);
+        assertThat(result)
+                .isCloseTo(expect, within(0.01));
     }
 
     @Test
     public void testNegativeData() {
-        assertThat(Point.distance(-77, -45, -63, -92))
-                .isCloseTo(49.1, within(0.1));
+        Point first = new Point(-77, -45);
+        Point second = new Point(-63, -92);
+        double result = 49.04;
+        double expect = second.distance(first);
+        assertThat(result)
+                .isCloseTo(expect, within(0.01));
     }
 }
