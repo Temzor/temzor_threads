ad2b0da77ffda488834104e0eacb818490331b3b
d.kapustin <onewaykd@gmail.com>
2023-11-01 11:25:10 +0300

    4. Расстояние между точками в трехмерном пространстве. https://job4j.ru/profile/exercise/20/task-view/144

--- src/main/java/ru/j4j/condition/Point.java
+++ src/main/java/ru/j4j/condition/Point.java
@@ -1,8 +1,17 @@
 package ru.j4j.condition;
 
+import static java.lang.Math.*;
+
 public class Point {
     private final int x;
     private final int y;
+    private int z;
+
+    public Point(int x, int y, int z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
 
     public Point(int x, int y) {
         this.x = x;
@@ -10,6 +19,10 @@ public class Point {
     }
 
     public double distance(Point that) {
-        return Math.sqrt(Math.pow(this.x - that.x, 2) + Math.pow(this.y - that.y, 2));
+        return sqrt(pow(this.x - that.x, 2) + pow(this.y - that.y, 2));
+    }
+
+    public double distance3D(Point that) {
+        return sqrt(pow(this.x - that.x, 2) + pow(this.y - that.y, 2) + pow(this.z - that.z, 2));
     }
 }
--- src/test/java/ru/j4j/condition/PointTest.java
+++ src/test/java/ru/j4j/condition/PointTest.java
@@ -24,4 +24,23 @@ class PointTest {
         assertThat(result)
                 .isCloseTo(expect, within(0.01));
     }
+    @Test
+    public void testPositiveData3D() {
+        Point first = new Point(10, 20, 30);
+        Point second = new Point(15, 30, 45);
+        double result = 18.7;
+        double expect = first.distance3D(second);
+        assertThat(result)
+                .isCloseTo(expect, within(0.01));
+    }
+
+    @Test
+    public void testNegativeData3D() {
+        Point first = new Point(-16, -32, -48);
+        Point second = new Point(-55, -45, -35);
+        double result = 43.11;
+        double expect = second.distance3D(first);
+        assertThat(result)
+                .isCloseTo(expect, within(0.01));
+    }
 }
