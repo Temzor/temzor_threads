b56eaa8c4c6a2212415281070d1bb54f7d275b68
temzor <onewaykd@gmail.com>
2023-11-28 17:57:00 +0300

    8. Date. Отображение даты; https://job4j.ru/profile/exercise/21/task-view/152

--- src/main/java/ru/j4j/tracker/Item.java
+++ src/main/java/ru/j4j/tracker/Item.java
@@ -1,5 +1,7 @@
 package ru.j4j.tracker;
 
+import java.time.LocalDateTime;
+
 public class Item {
     private int id;
     private String name;
@@ -18,5 +20,10 @@ public class Item {
 
     public void setName(String name) {
         this.name = name;
+    }
+    private final LocalDateTime created = LocalDateTime.now();
+
+    public LocalDateTime getCreated() {
+        return created;
     }
 }
---
+++ src/main/java/ru/j4j/tracker/StartUI.java
@@ -0,0 +1,13 @@
+package ru.j4j.tracker;
+
+import java.time.format.DateTimeFormatter;
+
+public class StartUI {
+    public static void main(String[] args) {
+        Item item = new Item();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
+        String out = item.getCreated().format(formatter);
+        System.out.println(out);
+    }
+}
+
