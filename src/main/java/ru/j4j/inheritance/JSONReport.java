5625ab81e1533d8fbd5374bed176b3d8236f54e8
temzor <onewaykd@gmail.com>
2023-11-28 17:43:53 +0300

    7. Аннотация @Override; https://job4j.ru/profile/exercise/21/task-view/151

--- src/main/java/ru/j4j/inheritance/JSONReport.java
+++ src/main/java/ru/j4j/inheritance/JSONReport.java
@@ -1,6 +1,7 @@
 package ru.j4j.inheritance;
 
-public class JSONReport {
+public class JSONReport extends TextReport {
+    @Override
     public String generate(String name, String body) {
         String ln = System.lineSeparator();
         return String.format("{" + ln
