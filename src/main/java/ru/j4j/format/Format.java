8461b41487ebfc77c4eba9a6e38c960404ce1a43
temzor <onewaykd@gmail.com>
2023-11-29 12:27:43 +0300

    9. Форматирование данных. Методы format() и printf() https://job4j.ru/profile/exercise/22/task-view/1055

---
+++ src/main/java/ru/j4j/format/Format.java
@@ -0,0 +1,11 @@
+package ru.j4j.format;
+
+public class Format {
+    public static void getGreeting(String name) {
+        System.out.println(String.format("Привет! Я %s", name));
+    }
+
+    public static void main(String[] args) {
+        getGreeting("Елена");
+    }
+}
