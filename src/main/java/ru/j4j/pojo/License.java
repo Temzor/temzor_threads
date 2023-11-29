066a026300621908c17e11ad48819045f976a7db
temzor <onewaykd@gmail.com>
2023-11-29 09:59:15 +0300

    4. Сравнение моделей. Метод equals; https://job4j.ru/profile/exercise/22/task-view/158

--- src/main/java/ru/j4j/pojo/License.java
+++ src/main/java/ru/j4j/pojo/License.java
@@ -1,6 +1,7 @@
 package ru.j4j.pojo;
 
 import java.util.Date;
+import java.util.Objects;
 
 public class License {
     private String owner;
@@ -38,6 +39,38 @@ public class License {
 
     public void setCreated(Date created) {
         this.created = created;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        License license = (License) o;
+
+        if (!Objects.equals(owner, license.owner)) {
+            return false;
+        }
+        if (!Objects.equals(model, license.model)) {
+            return false;
+        }
+        if (!Objects.equals(code, license.code)) {
+            return false;
+        }
+        return Objects.equals(created, license.created);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = owner != null ? owner.hashCode() : 0;
+        result = 31 * result + (model != null ? model.hashCode() : 0);
+        result = 31 * result + (code != null ? code.hashCode() : 0);
+        result = 31 * result + (created != null ? created.hashCode() : 0);
+        return result;
     }
 }
 
--- src/main/java/ru/j4j/pojo/Product.java
+++ src/main/java/ru/j4j/pojo/Product.java
@@ -1,5 +1,7 @@
 package ru.j4j.pojo;
 
+import java.util.Objects;
+
 public class Product {
     private final String name;
     private final int count;
@@ -15,5 +17,29 @@ public class Product {
 
     public int getCount() {
         return count;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        Product product = (Product) o;
+
+        if (count != product.count) {
+            return false;
+        }
+        return Objects.equals(name, product.name);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = name != null ? name.hashCode() : 0;
+        result = 31 * result + count;
+        return result;
     }
 }
