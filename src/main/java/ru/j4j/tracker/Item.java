96bc9e924e003e6893db2f1b6f177921596353b4
temzor <onewaykd@gmail.com>
2023-11-29 10:49:05 +0300

    5. Tracker - хранилище; https://job4j.ru/profile/exercise/22/task-view/159

--- src/main/java/ru/j4j/tracker/Item.java
+++ src/main/java/ru/j4j/tracker/Item.java
@@ -1,10 +1,26 @@
 package ru.j4j.tracker;
 
 import java.time.LocalDateTime;
+import java.util.Objects;
 
 public class Item {
     private int id;
+
     private String name;
+
+    private final LocalDateTime created = LocalDateTime.now();
+
+    public Item() {
+    }
+
+    public Item(String name) {
+        this.name = name;
+    }
+
+    public Item(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
 
     public int getId() {
         return id;
@@ -21,9 +37,36 @@ public class Item {
     public void setName(String name) {
         this.name = name;
     }
-    private final LocalDateTime created = LocalDateTime.now();
 
     public LocalDateTime getCreated() {
         return created;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        Item item = (Item) o;
+
+        if (id != item.id) {
+            return false;
+        }
+        if (!Objects.equals(name, item.name)) {
+            return false;
+        }
+        return Objects.equals(created, item.created);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id;
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        result = 31 * result + (created != null ? created.hashCode() : 0);
+        return result;
     }
 }
