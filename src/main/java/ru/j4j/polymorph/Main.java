412374a3f8addb01c967183d5b62c476ba182a7d
temzor <onewaykd@gmail.com>
2023-11-30 18:59:31 +0300

    4.0. Что такое Interface; https://job4j.ru/profile/exercise/23/task-view/1026

---
+++ src/main/java/ru/j4j/polymorph/Main.java
@@ -0,0 +1,25 @@
+package ru.j4j.polymorph;
+
+public class Main {
+    public static void main(String[] args) {
+        Vehicle townCar = new TownCar();
+        SportCar sportCar = new SportCar();
+        System.out.println("-=Town car=-");
+        townCar.changeGear();
+        townCar.accelerate();
+        townCar.steer();
+        townCar.brake();
+        System.out.println("-=Sport car=-");
+        sportCar.changeGear();
+        sportCar.accelerate();
+        sportCar.steer();
+        sportCar.brake();
+        sportCar.refill();
+
+        ((Vehicle) sportCar).changeGear();
+        ((Vehicle) sportCar).accelerate();
+        ((Vehicle) sportCar).steer();
+        ((Vehicle) sportCar).brake();
+        ((Fuel) sportCar).refill();
+    }
+}
---
+++ src/main/java/ru/j4j/polymorph/Vehicle.java
@@ -0,0 +1,11 @@
+package ru.j4j.polymorph;
+
+public interface Vehicle extends Fuel {
+    void accelerate();
+
+    void brake();
+
+    void steer();
+
+    void changeGear();
+}
