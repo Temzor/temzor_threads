fef34433451498284c7ba1ac7921298f7e21ebf2
Капустин Дмитрий <onewaykd@gmail.com>
2023-12-01 18:58:38 +0300

    8. Input и полиморфизм; https://job4j.ru/profile/exercise/23/task-view/181

--- src/main/java/ru/j4j/tracker/MockInput.java
+++ src/main/java/ru/j4j/tracker/MockInput.java
@@ -1,13 +1,20 @@
 package ru.j4j.tracker;
 
 public class MockInput implements Input {
+    private final String[] answers;
+    private int position = 0;
+
+    public MockInput(String[] answers) {
+        this.answers = answers;
+    }
+
     @Override
     public String askStr(String question) {
-        return null;
+        return answers[position++];
     }
 
     @Override
     public int askInt(String question) {
-        return 0;
+        return Integer.parseInt(askStr(question));
     }
 }
