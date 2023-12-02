f71d5b1353a2786728d148a8e70098a305d55331
Капустин Дмитрий <onewaykd@gmail.com>
2023-12-02 08:18:41 +0300

    9.0. Программируемые объекты для тестирования; https://job4j.ru/profile/exercise/23/task-view/1198

--- src/test/java/ru/j4j/tracker/StartUITest.java
+++ src/test/java/ru/j4j/tracker/StartUITest.java
@@ -6,12 +6,16 @@ import static org.assertj.core.api.Assertions.assertThat;
 class StartUITest {
     @Test
     void whenAddItem() {
-        String[] answers = {"Fix PC"};
+        String[] answers = {"Fix PC", "Fix Bug"};
         Input input = new MockInput(answers);
         Tracker tracker = new Tracker();
         StartUI.createItem(input, tracker);
+        StartUI.createItem(input, tracker);
         Item created = tracker.findAll()[0];
         Item expected = new Item("Fix PC");
+        assertThat(created.getName()).isEqualTo(expected.getName());
+        created = tracker.findAll()[1];
+        expected = new Item("Fix Bug");
         assertThat(created.getName()).isEqualTo(expected.getName());
     }
 }
