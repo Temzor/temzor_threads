5711893aa76ffaadb862ea4d4f0c3fa54f32ac48
d.kapustin <onewaykd@gmail.com>
2023-12-05 16:19:15 +0300

    3. final; https://job4j.ru/profile/exercise/25/task-view/205

---
+++ src/main/java/ru/j4j/ooa/Aircraft.java
@@ -0,0 +1,8 @@
+package ru.j4j.ooa;
+
+public class Aircraft {
+
+    public void printModel() {
+        System.out.println("Метод выводит в консоль модель самолета.");
+    }
+}
---
+++ src/main/java/ru/j4j/ooa/Airport.java
@@ -0,0 +1,18 @@
+package ru.j4j.ooa;
+
+public class Airport {
+    public static void main(String[] args) {
+        final Airbus airbus = new Airbus("A320");
+        System.out.println(airbus);
+        airbus.printModel();
+        airbus.printCountEngine();
+
+        airbus.setName("A380");
+        System.out.println(airbus);
+        airbus.printCountEngine();
+
+        airbus.setName("A200");
+        System.out.println(airbus);
+        airbus.printCountEngine();
+    }
+}
