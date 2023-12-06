6fa924500c2495efbc4fc96bd8685a3af33e7e76
d.kapustin <onewaykd@gmail.com>
2023-12-06 12:15:09 +0300

    3. Автоупаковка / автораспаковка; https://job4j.ru/profile/exercise/27/task-view/1051

---
+++ src/main/java/ru/j4j/collection/Box.java
@@ -0,0 +1,10 @@
+package ru.j4j.collection;
+
+public class Box {
+    public static void main(String[] args) {
+        int a = 1;
+        Integer b;
+        b = a;
+        System.out.println("Integer b = " + b);
+    }
+}
