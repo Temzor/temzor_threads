af60b608581fc30445ea5413642d7cd14f05c001
d.kapustin <onewaykd@gmail.com>
2023-12-11 18:49:48 +0300

    0. Сортировка; https://job4j.ru/profile/exercise/30/task/224/34673

---
+++ src/main/java/ru/j4j/collection/JobSorter.java
@@ -0,0 +1,17 @@
+package ru.j4j.collection;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class JobSorter {
+    public static void main(String[] args) {
+        List<Job> jobs = Arrays.asList(
+                new Job("Fix bugs", 4),
+                new Job("Impl task", 2),
+                new Job("Reboot server", 1)
+        );
+        System.out.println(jobs);
+        jobs.sort(new SortDescByNameJob());
+        System.out.println(jobs);
+    }
+}
---
+++ src/test/java/ru/j4j/tracker/comporator/ItemAscByNameTest.java
@@ -0,0 +1,30 @@
+package ru.j4j.tracker.comporator;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import ru.j4j.tracker.model.Item;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class ItemAscByNameTest {
+
+    @Test
+    void compareAscItem() {
+        ItemAscByName itemAscByName = new ItemAscByName();
+        List<Item> result = new ArrayList<>(List.of(
+                new Item("abc"),
+                new Item("gjk"),
+                new Item("def")
+        ));
+
+        List<Item> expect = List.of(
+                new Item("abc"),
+                new Item("def"),
+                new Item("gjk")
+        );
+        result.sort(new ItemAscByName());
+        assertThat(result).containsAll(expect);
+    }
+}
