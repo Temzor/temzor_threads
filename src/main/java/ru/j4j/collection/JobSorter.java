3f3c30925c80b5d7744a80c05b424898dce38f8e
d.kapustin <onewaykd@gmail.com>
2023-12-13 13:41:08 +0300

    2. Комбинированный компаратор; https://job4j.ru/profile/exercise/30/task-view/226

--- src/main/java/ru/j4j/collection/JobSorter.java
+++ src/main/java/ru/j4j/collection/JobSorter.java
@@ -1,17 +1,20 @@
 package ru.j4j.collection;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class JobSorter {
     public static void main(String[] args) {
         List<Job> jobs = Arrays.asList(
-                new Job("Fix bugs", 4),
-                new Job("Impl task", 2),
-                new Job("Reboot server", 1)
+                new Job("Fix bug", 1),
+                new Job("Fix bug", 4),
+                new Job("Fix bug", 2),
+                new Job("X task", 0)
         );
-        System.out.println(jobs);
-        jobs.sort(new SortDescByNameJob());
+        Collections.sort(jobs, new JobDescByName()
+                .thenComparing(new JobDescByPriority())
+                .thenComparing(new JobDescByNameLength()));
         System.out.println(jobs);
     }
 }
