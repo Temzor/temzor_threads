0493d991f50c27e83aa448a256f7151826b080c9
Капустин Дмитрий <onewaykd@gmail.com>
2023-12-14 15:23:29 +0300

    4. Сортировка номера; https://job4j.ru/profile/exercise/30/task-view/228

---
+++ src/main/java/ru/j4j/collection/LexSort.java
@@ -0,0 +1,14 @@
+package ru.j4j.collection;
+
+import java.util.Comparator;
+
+public class LexSort implements Comparator<String> {
+    @Override
+    public int compare(String left, String right) {
+        String[] leftSplit = left.split(". ");
+        String[] rightSplit = right.split(". ");
+        int leftInt = Integer.parseInt(leftSplit[0]);
+        int rightInt = Integer.parseInt(rightSplit[0]);
+        return Integer.compare(leftInt, rightInt);
+    }
+}
