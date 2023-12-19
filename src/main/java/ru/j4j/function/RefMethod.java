7364bf04bba4fa39bd68c7bdff999cb5f0250d36
d.kapustin <onewaykd@gmail.com>
2023-12-19 17:33:09 +0300

    3. Ссылки на методы.

---
+++ src/main/java/ru/j4j/function/RefMethod.java
@@ -0,0 +1,25 @@
+package ru.j4j.function;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+
+public class RefMethod {
+    public static void main(String[] args) {
+        List<String> names = Arrays.asList(
+                "Ivan",
+                "Petr Arsentev"
+        );
+        Consumer<String> out = RefMethod::cutOut;
+        names.forEach(out);
+    }
+
+    public static void cutOut(String value) {
+        if (value.length() > 10) {
+            System.out.println(value.substring(0, 10) + "..");
+        } else {
+            System.out.println(value);
+        }
+    }
+}
+
