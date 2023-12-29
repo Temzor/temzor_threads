d0ba99792a8dd0f72faeb6465e0eb9992b436e34
d.kapustin <onewaykd@gmail.com>
2023-12-29 16:11:09 +0300

    7. Уникальность элементов и сортировка

--- src/main/java/ru/j4j/stream/Profiles.java
+++ src/main/java/ru/j4j/stream/Profiles.java
@@ -1,5 +1,6 @@
 package ru.j4j.stream;
 
+import java.util.Comparator;
 import java.util.List;
 
 public class Profiles {
@@ -7,6 +8,14 @@ public class Profiles {
     public static List<Address> collect(List<Profile> profiles) {
         return profiles.stream()
                 .map(Profile::getAddress)
+                .toList();
+    }
+
+    public static List<Address> collectSortWithoutDuplicate(List<Profile> profiles) {
+        return profiles.stream()
+                .map(Profile::getAddress)
+                .sorted(Comparator.comparing(Address::getCity))
+                .distinct()
                 .toList();
     }
 }
