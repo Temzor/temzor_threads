c3a79e0a1b67c638a8ee2588fd9607d0ea952d3e
d.kapustin <onewaykd@gmail.com>
2023-12-29 15:49:48 +0300

    6. Список адресов

--- src/main/java/ru/j4j/stream/Profiles.java
+++ src/main/java/ru/j4j/stream/Profiles.java
@@ -6,8 +6,7 @@ public class Profiles {
 
     public static List<Address> collect(List<Profile> profiles) {
         return profiles.stream()
-                .map(profile -> new Address(profile.getAddress().getCity(), profile.getAddress().getStreet(),
-                        profile.getAddress().getHome(), profile.getAddress().getApartment()))
+                .map(Profile::getAddress)
                 .toList();
     }
 }
