fc0b93838ecc5a583e41b55c9873d448d582b562
d.kapustin <onewaykd@gmail.com>
2023-12-29 12:26:50 +0300

    Closing in Sum

--- src/main/java/ru/code/seven/ClosingInSum.java
+++ src/main/java/ru/code/seven/ClosingInSum.java
@@ -1,18 +1,11 @@
 package ru.code.seven;
+import static java.util.stream.IntStream.range;
 
 public class ClosingInSum {
     public static int closingInSum(long n) {
-        String[] stringsSplit = Long.toString(n).split("");
-        int result = 0;
-        int reverseCounter = stringsSplit.length;
-        for (int i = 0; i < reverseCounter; i++) {
-            if (i == reverseCounter - 1) {
-                result += Integer.parseInt(stringsSplit[i]);
-            } else {
-                result += Integer.parseInt(stringsSplit[i] + stringsSplit[reverseCounter - 1]);
-                reverseCounter--;
-            }
-        }
-        return result;
+        String s = String.valueOf(n);
+        return range(0, s.length())
+                .map(i -> (i < s.length() / 2 ? 10 : 1) * (s.charAt(i) - 48))
+                .sum();
     }
 }
