9fee3cba48c72a2cf4d4a96ca63133c7c35ac162
Капустин Дмитрий <onewaykd@gmail.com>
2023-12-31 10:31:23 +0300

    fix test RainTacosTest

--- src/test/java/ru/code/six/RainTacosTest.java
+++ src/test/java/ru/code/six/RainTacosTest.java
@@ -1,25 +1,22 @@
 package ru.code.six;
 
 import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-class SolutionTest {
+class RainTacosTest {
     @Test
     void sampleTests() {
-        // Test an empty string
         assertEquals("", RainTacos.rainTacos(""));
-        // Test a flat landscape
         assertEquals("     \nTACOT\nOOOOO", RainTacos.rainTacos("     \n     \nOOOOO"));
-        // Test a landscape with no tacos
         assertEquals("OOOOO\nOOOOO\nOOOOO", RainTacos.rainTacos("OOOOO\nOOOOO\nOOOOO"));
-        // Test a landscape with a taco island
-        assertEquals("       \n   O   \n  COT  \n AOOOA \nTTACOSC", RainTacos.rainTacos("       \n       \n   O   \n  OOO  \n TACOS "));
-        // Test a deep trench
-        assertEquals("* *\n* *\n* *\n* *\n* *\n* *\n* *\n* *\n* *\n*A*", RainTacos.rainTacos("* *\n* *\n* *\n* *\n* *\n* *\n* *\n* *\n* *\n* *"));
-        // Test a long landscape
-        assertEquals("TACOTACOTACOTA\n--------------", RainTacos.rainTacos("              \n--------------"));
-        // Test a floating island
-        assertEquals("    TA    \n    ==    \n          \n          \nTACO  COTA", RainTacos.rainTacos("          \n    ==    \n          \n          \n          "));
+        assertEquals("       \n   O   \n  COT  \n AOOOA \nTTACOSC", RainTacos.rainTacos(
+                "       \n       \n   O   \n  OOO  \n TACOS "));
+        assertEquals("* *\n* *\n* *\n* *\n* *\n* *\n* *\n* *\n* *\n*A*",
+                RainTacos.rainTacos("* *\n* *\n* *\n* *\n* *\n* *\n* *\n* *\n* *\n* *"));
+        assertEquals("TACOTACOTACOTA\n--------------", RainTacos.rainTacos("              "
+                + "\n--------------"));
+        assertEquals("    TA    \n    ==    \n          \n          \nTACO  COTA",
+                RainTacos.rainTacos("          \n    ==    \n          \n          \n          "));
     }
 }
-
