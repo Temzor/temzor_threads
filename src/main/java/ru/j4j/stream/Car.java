dcf2ac67b333a4072cb18fae97ffc6972e4866cd
Капустин Дмитрий <onewaykd@gmail.com>
2023-12-31 11:47:47 +0300

    18. Шаблоны проектирования в Stream API https://job4j.ru/profile/exercise/3/task-view/259

---
+++ src/main/java/ru/j4j/stream/Car.java
@@ -0,0 +1,82 @@
+package ru.j4j.stream;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.time.LocalDate;
+@ToString
+@Setter
+@Getter
+public class Car {
+    private String brand;
+
+    private String model;
+
+    private LocalDate created;
+
+    private double volume;
+
+    private String color;
+
+    static class Builder {
+        private String brand;
+        private String model;
+        private LocalDate created;
+        private double volume;
+        private String color;
+
+        Builder buildBrand(String brand) {
+            this.brand = brand;
+            return this;
+        }
+
+        Builder buildModel(String model) {
+            this.model = model;
+            return this;
+        }
+
+        Builder buildCreated(LocalDate created) {
+            this.created = created;
+            return this;
+        }
+
+        Builder buildVolume(double volume) {
+            this.volume = volume;
+            return this;
+        }
+
+        Builder buildColor(String color) {
+            this.color = color;
+            return this;
+        }
+
+        Car build() {
+            Car car = new Car();
+            car.brand = brand;
+            car.model = model;
+            car.created = created;
+            car.volume = volume;
+            car.color = color;
+            return car;
+        }
+    }
+
+    public static void main(String[] args) {
+        Car car = new Builder()
+                .buildBrand("Toyota")
+                .buildModel("Camry")
+                .buildCreated(LocalDate.of(2021, 6, 1))
+                .buildVolume(2.5)
+                .buildColor("Red")
+                .build();
+        System.out.println(car);
+
+
+        Car car1 = new Builder()
+                .buildBrand("Toyota")
+                .buildModel("Camry")
+                .build();
+        System.out.println(car1);
+    }
+}
