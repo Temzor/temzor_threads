fef82b6700d02a662e50843c3cc8d79c45e9dc8b
d.kapustin <onewaykd@gmail.com>
2024-01-11 15:56:57 +0300

    5. Внедрение зависимостей. Dependency Injection (DI). Введение

--- src/main/java/ru/spring/SpringStart.java
+++ src/main/java/ru/spring/SpringStart.java
@@ -7,9 +7,7 @@ public class SpringStart {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
                 "applicationContext.xml"
         );
-
-        Music music = context.getBean("musicBean", Music.class);
-        MusicPlayer musicPlayer = new MusicPlayer(music);
+        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
         musicPlayer.playMusic();
 
         context.close();
