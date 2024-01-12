cf072e5c24a3fdc5d8e8fa9c1247896d32a3c178
d.kapustin <onewaykd@gmail.com>
2024-01-12 13:49:31 +0300

    8. Жизненный цикл бина (Bean Lifecycle). Init, Destroy и Factory методы

--- src/main/java/ru/spring/ClassicalMusic.java
+++ src/main/java/ru/spring/ClassicalMusic.java
@@ -1,6 +1,25 @@
 package ru.spring;
 
+import lombok.AccessLevel;
+
 public class ClassicalMusic implements Music {
+
+    private ClassicalMusic() {
+
+    }
+
+    public static ClassicalMusic getInstance() {
+        return new ClassicalMusic();
+    }
+
+    public void classicalMusicBeanInit() {
+        System.out.println("Initialization bean: " + ClassicalMusic.class.getName());
+    }
+
+    public void classicalMusicBeanDestroy() {
+        System.out.println("Destroying bean: " + ClassicalMusic.class.getName());
+    }
+
     @Override
     public String getSong() {
         return "Moon Rhapsody";
