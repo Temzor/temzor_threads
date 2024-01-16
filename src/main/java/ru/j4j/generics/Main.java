2a4feee529d36fe17835651d47ea2a2a7178fb88
d.kapustin <onewaykd@gmail.com>
2024-01-16 15:26:17 +0300

    Модуль 0. Урок 1. Знакомство с generic в Java.

--- src/main/java/ru/j4j/generics/Main.java
+++ src/main/java/ru/j4j/generics/Main.java
@@ -2,10 +2,18 @@ package ru.j4j.generics;
 
 public class Main {
     public static void main(String[] args) {
-        final Tuple studentFirst = new Tuple(1, "Niki");
-        final String name = studentFirst.getRight().toString();
-        final String nameNext = (String) studentFirst.getRight();
-        final String nameMistakes = (String) studentFirst.getLeft();
-        System.out.println(nameMistakes);
+        final Tuple<String> stringTupleFirst = new Tuple<>("l", "r");
+        final Tuple<Integer> integerTuple = new Tuple<>(1, 2);
+        final Tuple<SuperClass> superClassTuple = new Tuple<>(new SuperClass(), new SuperClass());
+        System.out.println(stringTupleFirst.getLeft());
+        System.out.println(integerTuple.getRight() + 15);
+        System.out.println(superClassTuple.getRight());
+    }
+
+    private static class SuperClass {
+        @Override
+        public String toString() {
+            return "super";
+        }
     }
 }
--- src/main/java/ru/j4j/generics/Tuple.java
+++ src/main/java/ru/j4j/generics/Tuple.java
@@ -2,11 +2,10 @@ package ru.j4j.generics;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.Setter;
 
 @AllArgsConstructor
 @Getter
-public class Tuple {
-    private final Object left;
-    private final Object right;
+public class Tuple<T> {
+    private final T left;
+    private final T right;
 }
