59d8e2f3e7328e0007cedf15e14a2d27bcb5f3a6
d.kapustin <onewaykd@gmail.com>
2024-02-05 18:11:11 +0300

    Send in the Clones; 7kyu.

--- src/main/java/ru/code/seven/DingleMouse.java
+++ src/main/java/ru/code/seven/DingleMouse.java
@@ -1,17 +1,10 @@
 package ru.code.seven;
 
 public class DingleMouse {
-    public static long[] clonewars(final int kataPerDay) {
-        int numberOfClones = 1;
-        int numberOfKataSolvedByClones = 0;
-        int numberOfKatasPerDay = kataPerDay;
-
-        for (int i = 1; i <= kataPerDay; i++) {
-            numberOfKataSolvedByClones += numberOfClones * numberOfKatasPerDay;
-            numberOfClones *= 2;
-            numberOfKatasPerDay--;
-        }
-
-        return kataPerDay < 1 ? new long[] {1, 0} : new long[] {numberOfClones / 2, numberOfKataSolvedByClones};
+    public static long[] clonewars(int kataPerDay) {
+        return new long[] {
+                (long) Math.ceil(Math.pow(2, kataPerDay - 1)),
+                (long) Math.pow(2, kataPerDay + 1) - kataPerDay - 2
+        };
     }
 }
