73a4eed0caf86cb1cc8e2198524b48757451a04e
d.kapustin <onewaykd@gmail.com>
2024-02-12 15:20:27 +0300

    Reversing Fun; 7kyu.

---
+++ src/main/java/ru/code/seven/FunReverse.java
@@ -0,0 +1,17 @@
+package ru.code.seven;
+
+
+public class FunReverse {
+    public static String funReverse(String s) {
+        StringBuilder reverse = new StringBuilder(s).reverse();
+        StringBuilder reverseFinal = new StringBuilder();
+
+        for (int i = 0; i < s.length(); i++) {
+            reverseFinal.append(reverse.charAt(0));
+            reverse.deleteCharAt(0);
+            reverse.reverse();
+
+        }
+        return reverseFinal.toString();
+    }
+}
---
+++ src/test/java/ru/code/seven/FunReverseTest.java
@@ -0,0 +1,22 @@
+package ru.code.seven;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class FunReverseTest {
+    private static void testing(String actual, String expected) {
+        assertThat(expected).isEqualTo(actual);
+    }
+
+    @Test
+    public void test() {
+        System.out.println("Fixed Tests funReverse");
+        testing(FunReverse.funReverse("012"), "201");
+        testing(FunReverse.funReverse("012345"), "504132");
+        testing(FunReverse.funReverse("0123456789"), "9081726354");
+        testing(FunReverse.funReverse("Hello"), "oHlel");
+        testing(FunReverse.funReverse("4ppso1vdjc9rjyf5bkmd5nztr8"), "84rptpzsno51dvmdkjbc59fryj");
+    }
+
+}
