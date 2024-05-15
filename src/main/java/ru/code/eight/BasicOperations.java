497aa6dfd5463d2c2462f01a150edd58a50de76e
Капустин Дмитрий <onewaykd@gmail.com>
2024-05-15 22:37:54 +0300

    8 kyu BasicOperationsTest

--- src/main/java/ru/code/eight/BasicOperations.java
+++ src/main/java/ru/code/eight/BasicOperations.java
@@ -2,12 +2,22 @@ package ru.code.eight;
 
 public class BasicOperations {
     public static Integer basicMath(String op, int x, int y) {
-        return switch (op) {
-            case "+" -> x + y;
-            case "-" -> x - y;
-            case "*" -> x * y;
-            case "/" -> x / y;
-            default -> 0;
-        };
+        switch (op) {
+            case "+" -> {
+                return x + y;
+            }
+            case "-" -> {
+                return x - y;
+            }
+            case "*" -> {
+                return x * y;
+            }
+            case "/" -> {
+                return x / y;
+            }
+            default -> {
+                return 0;
+            }
+        }
     }
 }
