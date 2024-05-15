d728e243f478666ea802a1e3aeeb901f6904d1de
Капустин Дмитрий <onewaykd@gmail.com>
2024-05-15 22:42:07 +0300

    8 kyu BasicOperationsTest fix bugs

--- src/main/java/ru/code/eight/BasicOperations.java
+++ src/main/java/ru/code/eight/BasicOperations.java
@@ -2,22 +2,12 @@ package ru.code.eight;
 
 public class BasicOperations {
     public static Integer basicMath(String op, int x, int y) {
-        switch (op) {
-            case "+" -> {
-                return x + y;
-            }
-            case "-" -> {
-                return x - y;
-            }
-            case "*" -> {
-                return x * y;
-            }
-            case "/" -> {
-                return x / y;
-            }
-            default -> {
-                return 0;
-            }
-        }
+        return switch (op) {
+            case "+" -> x + y;
+            case "-" -> x - y;
+            case "*" -> x * y;
+            case "/" -> x / y;
+            default -> 0;
+        };
     }
 }
--- src/test/java/ru/code/eight/BasicOperationsTest.java
+++ src/test/java/ru/code/eight/BasicOperationsTest.java
@@ -6,13 +6,11 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class BasicOperationsTest {
     @Test
-            void basicOperations()
-    {
+    void basicOperations() {
         System.out.println("Basic Tests");
-        assertThat(BasicOperations.basicMath("+", 4, 7).equals(11));
-        assertThat(BasicOperations.basicMath("-", 15, 18).equals(-3));
-        assertThat(BasicOperations.basicMath("*", 5, 5).equals(25));
-        assertThat(BasicOperations.basicMath("/", 49, 7).equals(7));
+        assertThat(11).isEqualTo(BasicOperations.basicMath("+", 4, 7));
+        assertThat(-3).isEqualTo(BasicOperations.basicMath("-", 15, 18));
+        assertThat(25).isEqualTo(BasicOperations.basicMath("-", 15, 18));
+        assertThat(7).isEqualTo(BasicOperations.basicMath("/", 49, 7));
     }
-
 }
