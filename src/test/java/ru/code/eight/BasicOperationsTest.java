3a91f0b67242bbe347adf0376d7f93a6584b5ecf
Капустин Дмитрий <onewaykd@gmail.com>
2024-05-15 22:44:27 +0300

    8 kyu BasicOperationsTest fix bugs

--- src/test/java/ru/code/eight/BasicOperationsTest.java
+++ src/test/java/ru/code/eight/BasicOperationsTest.java
@@ -10,7 +10,7 @@ class BasicOperationsTest {
         System.out.println("Basic Tests");
         assertThat(11).isEqualTo(BasicOperations.basicMath("+", 4, 7));
         assertThat(-3).isEqualTo(BasicOperations.basicMath("-", 15, 18));
-        assertThat(25).isEqualTo(BasicOperations.basicMath("-", 15, 18));
+        assertThat(25).isEqualTo(BasicOperations.basicMath("-", 40, 15));
         assertThat(7).isEqualTo(BasicOperations.basicMath("/", 49, 7));
     }
 }
