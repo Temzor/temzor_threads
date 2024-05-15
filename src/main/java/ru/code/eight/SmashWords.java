c4b563129940ca787f37c9d48f0fd09c22a3c02e
Капустин Дмитрий <onewaykd@gmail.com>
2024-05-15 22:28:07 +0300

    8 kyu Sentence Smash

--- src/main/java/ru/code/eight/SmashWords.java
+++ src/main/java/ru/code/eight/SmashWords.java
@@ -2,14 +2,6 @@ package ru.code.eight;
 
 public class SmashWords {
     public static String smash(String... words) {
-        StringBuilder smashed = new StringBuilder();
-        for (int i = 0; i < words.length; i++) {
-            if (i == words.length - 1) {
-                smashed.append(words[i]);
-                break;
-            }
-            smashed.append(words[i]).append(" ");
-        }
-        return smashed.toString();
+        return String.join(" ", words);
     }
 }
