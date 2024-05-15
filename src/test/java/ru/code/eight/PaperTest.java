a59092b891c8afa33f1134a5c691af721b687aad
Капустин Дмитрий <onewaykd@gmail.com>
2024-05-15 22:33:42 +0300

    8 kyu Sentence Smash

--- src/test/java/ru/code/eight/PaperTest.java
+++ src/test/java/ru/code/eight/PaperTest.java
@@ -8,7 +8,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 class PaperTest {
     @Test
     void test() {
-        assertThat(25).isEqualTo(Paper.paperWork(5,5));
-        assertThat(0).isEqualTo(Paper.paperWork(-5,5));
+        assertThat(25).isEqualTo(Paper.paperWork(5, 5));
+        assertThat(0).isEqualTo(Paper.paperWork(-5, 5));
     }
 }
