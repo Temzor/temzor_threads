e764a66982f154499452ec8ad0e81618f931133f
Dmitrii <temzor@mail.ru>
2024-05-16 12:22:17 +0300

    8 kyu; My head is at the wrong end.

--- src/main/java/ru/code/eight/WrongEndHead.java
+++ src/main/java/ru/code/eight/WrongEndHead.java
@@ -3,13 +3,7 @@ package ru.code.eight;
 public class WrongEndHead {
 
     public static String[] fixTheMeerkat(String[] arr) {
-        String[] result = new String[arr.length];
-        int x = 0;
-        for (int i = arr.length - 1; i >= 0; i--) {
-            result[x] = arr[i];
-            x++;
-        }
-        return result;
+        return new String[] {arr[2], arr[1], arr[0]};
     }
 }
 
