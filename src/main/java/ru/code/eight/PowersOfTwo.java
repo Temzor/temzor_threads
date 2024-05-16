e8bb333c067cc271c3272e715241c4252118afba
Капустин Дмитрий <onewaykd@gmail.com>
2024-05-16 19:30:39 +0300

    8 kyu; Powers of 2.

--- src/main/java/ru/code/eight/PowersOfTwo.java
+++ src/main/java/ru/code/eight/PowersOfTwo.java
@@ -1,12 +1,9 @@
 package ru.code.eight;
 
+import static java.util.stream.LongStream.rangeClosed;
+
 public class PowersOfTwo {
     public static long[] powersOfTwo(int n) {
-        long[] array = new long[n + 1];
-        array[0] = 1;
-        for (int i = 1; i <= n; i++) {
-            array[i] = 2 * array[i - 1];
-        }
-        return array;
+        return rangeClosed(0, n).map(i -> (long) Math.pow(2, i)).toArray();
     }
 }
