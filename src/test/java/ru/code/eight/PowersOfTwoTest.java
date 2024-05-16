34f3253edeb1ffeb67a9a4da6716028ff8e6c284
Капустин Дмитрий <onewaykd@gmail.com>
2024-05-16 21:32:33 +0300

    8 kyu; Fix mistakes in PowersOfTwoTest.java

--- src/test/java/ru/code/eight/PowersOfTwoTest.java
+++ src/test/java/ru/code/eight/PowersOfTwoTest.java
@@ -7,9 +7,9 @@ import static org.assertj.core.api.Assertions.assertThat;
 class PowersOfTwoTest {
     @Test
     void whenTest() {
-        assertThat(new long[] {1,2,4}).isEqualTo(PowersOfTwo.powersOfTwo(2));
+        assertThat(new long[]{1, 2, 4}).isEqualTo(PowersOfTwo.powersOfTwo(2));
         assertThat(new long[]{1}).isEqualTo(PowersOfTwo.powersOfTwo(0));
-        assertThat(new long[]{1,2}).isEqualTo(PowersOfTwo.powersOfTwo(1));
-        assertThat(new long[]{1,2,4,8,16}).isEqualTo(PowersOfTwo.powersOfTwo(4));
+        assertThat(new long[]{1, 2}).isEqualTo(PowersOfTwo.powersOfTwo(1));
+        assertThat(new long[]{1, 2, 4, 8, 16}).isEqualTo(PowersOfTwo.powersOfTwo(4));
     }
 }
