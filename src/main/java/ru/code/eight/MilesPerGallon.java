56e1f78cd0769eac32a0ebd6050f52cbd9dd13c0
Dmitrii <temzor@mail.ru>
2024-05-17 11:59:52 +0300

    8 kyu; Miles per gallon to kilometers per liter.

--- src/main/java/ru/code/eight/MilesPerGallon.java
+++ src/main/java/ru/code/eight/MilesPerGallon.java
@@ -2,6 +2,6 @@ package ru.code.eight;
 
 public class MilesPerGallon {
     public static float mpgToKPM(final float mpg) {
-        return (float) ((mpg * 1.609344) / 4.54609188);
+        return (float) (Math.round(((mpg / 4.54609188f) * 1.609344f) * 100.0) / 100.0);
     }
 }
