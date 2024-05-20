388208208a714e4d5baa387bf268e9a2bcd68ead
Капустин Дмитрий <onewaykd@gmail.com>
2024-05-20 14:58:08 +0300

    https://youtu.be/m-7EljqdxpA?si=n6CCQafE7pAZAfU8 11:21

---
+++ src/main/java/ru/code/concurrency/ServiceDemo.java
@@ -0,0 +1,13 @@
+package ru.code.concurrency;
+
+public class ServiceDemo {
+    public static void main(String[] args) {
+        Service service = new Service();
+
+        service.readData();
+        service.showGreetingMessage();
+        service.calculateFactorial(50);
+        service.calculateSum(100);
+        service.finishProgram();
+    }
+}
