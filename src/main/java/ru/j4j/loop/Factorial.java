d200bf0f4982cc22ed5bab4a200fbafa201c73dc
Капустин Дмитрий <onewaykd@gmail.com>
2024-05-30 15:31:27 +0300

    5.2. Создать программу, вычисляющую факториал. [#193 #34650]

--- src/main/java/ru/j4j/loop/Factorial.java
+++ src/main/java/ru/j4j/loop/Factorial.java
@@ -3,9 +3,9 @@ package ru.j4j.loop;
 public class Factorial {
     public static int calc(int n) {
         int result = 1;
-        for (int i = 1; i <= n; i++) {
-            result *= i;
+        for (int i = 0; i <= n; i++) {
+            result += result + i;
         }
-        return result;
+        return result - 1;
     }
 }
