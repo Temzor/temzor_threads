c956f4c445b8af3888f10ab8c497e4f2563f9346
Dmitrii <temzor@mail.ru>
2024-05-30 10:19:01 +0300

    4.1.2. Глупый бот [#31323 #34583]

--- src/main/java/ru/j4j/condition/DummyBot.java
+++ src/main/java/ru/j4j/condition/DummyBot.java
@@ -2,9 +2,9 @@ package ru.j4j.condition;
 
 public class DummyBot {
     public static String answer(String question) {
-        if (question.equals("Hi, bot.")) {
+        if ("Hi, bot.".equals(question)) {
             return "Hi, SmartAss.";
-        } else if (question.equals("Bye.")) {
+        } else if ("Bye.".equals(question)) {
             return "See you later.";
         } else {
             return "I don't know. Please, ask another question.";
