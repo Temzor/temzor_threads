423d15603471073ca74c2e2272d205007f4f2569
Капустин Дмитрий <onewaykd@gmail.com>
2024-05-30 22:46:25 +0300

    5.9. Крест в псевдографике.  [#177182 #34653]

--- src/main/java/ru/j4j/loop/Slash.java
+++ src/main/java/ru/j4j/loop/Slash.java
@@ -4,12 +4,12 @@ public class Slash {
     public static void draw(int size) {
         for (int row = 0; row < size; row++) {
             for (int cell = 0; cell < size; cell++) {
-                boolean left = row == cell; /* добавить условие для левой диагонали - с левого верхнего угла в правый нижний*/
-                boolean right = size - row - 1 == cell; /* добавить условие для правой диагонали - из левого нижнего в правый верхний*/
+                boolean left = row % 2 == 0 && cell % 2 == 0 || row % 2 == 1 && cell % 2 == 1;
+                boolean right = false;
                 if (left) {
-                    System.out.print("0");
+                    System.out.print("l");
                 } else if (right) {
-                    System.out.print("0");
+                    System.out.print("r");
                 } else {
                     System.out.print(" ");
                 }
@@ -22,7 +22,5 @@ public class Slash {
     public static void main(String[] args) {
         System.out.println("Draw by 3");
         draw(3);
-        System.out.println("Draw by 5");
-        draw(5);
     }
 }
