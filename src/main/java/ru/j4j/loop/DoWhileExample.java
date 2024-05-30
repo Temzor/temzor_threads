e745af927b25453e168e8d19bce85fe29047ac35
Капустин Дмитрий <onewaykd@gmail.com>
2024-05-30 22:56:34 +0300

    6.0. Цикл do-while [#504919 #447877]

--- src/main/java/ru/j4j/loop/DoWhileExample.java
+++ src/main/java/ru/j4j/loop/DoWhileExample.java
@@ -1,16 +1,21 @@
 package ru.j4j.loop;
 
+import java.util.NoSuchElementException;
 import java.util.Scanner;
 
 public class DoWhileExample {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int password;
-        do {
-            System.out.println("Enter password: ");
-            password = scanner.nextInt();
-        } while (password != 555);
-        System.out.println("Password enter is correct!");
+        try {
+            Scanner scanner = new Scanner(System.in);
+            int password;
+            do {
+                System.out.println("Enter password: ");
+                password = scanner.nextInt();
+            } while (password != 555);
+            System.out.println("Password enter is correct!");
+        } catch (NoSuchElementException e) {
+            System.out.println("Error: " + e.getCause() + e);
+        }
     }
 }
 
