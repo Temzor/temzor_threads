579722655a41c40f572561abf403569c5c842139
Капустин Дмитрий <onewaykd@gmail.com>
2024-05-31 22:08:57 +0300

    6.7.1. Моно строка в матрице. [#214126 #34561]

--- src/main/java/ru/j4j/array/MatrixCheck.java
+++ src/main/java/ru/j4j/array/MatrixCheck.java
@@ -2,37 +2,11 @@ package ru.j4j.array;
 
 public class MatrixCheck {
     public static boolean monoHorizontal(char[][] board, int row) {
-        for (int i = 0; i < board[row].length; i++) {
-            if (board[row][i] != 'X') {
+        for (int cell = 0; cell < board[row].length; cell++) {
+            if (board[row][cell] == ' ') {
                 return false;
             }
         }
         return true;
-    }
-
-    public static boolean monoVertical(char[][] board, int column) {
-        for (char[] chars : board) {
-            if (chars[column] != 'X') {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    public static char[] extractDiagonal(char[][] board) {
-        char[] boardChars = new char[board.length];
-        for (int i = 0; i < board.length; i++) {
-            boardChars[i] = board[i][i];
-        }
-        return boardChars;
-    }
-
-    public static boolean isWin(char[][] board) {
-        for (int i = 0; i < board.length; i++) {
-            if (monoHorizontal(board, i) || monoVertical(board, i)) {
-                return true;
-            }
-        }
-        return false;
     }
 }
