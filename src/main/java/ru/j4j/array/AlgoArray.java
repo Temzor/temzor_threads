35ce7c930a78cda9fa870a8f464fe66b3010127d
Капустин Дмитрий <onewaykd@gmail.com>
2024-05-31 14:40:42 +0300

    6.2. Упорядочить массив. [#4441 #34573]

--- src/main/java/ru/j4j/array/AlgoArray.java
+++ src/main/java/ru/j4j/array/AlgoArray.java
@@ -2,16 +2,19 @@ package ru.j4j.array;
 
 public class AlgoArray {
     public static void main(String[] args) {
-        int[] array = new int[] {5, 3, 2, 1, 4};
-        for (int i = 0; i < array.length - 1; i++) {
-            for (int j = 0; j < array.length - 1 - i; j++) {
-                if (array[j] == array[j + 1]) {
-                    int temp = array[j];
-                    array[j] = array[j + 1];
-                    array[j + 1] = temp;
-                }
-            }
-        }
+        int[] array = new int[]{5, 3, 2, 1, 4};
+        int temp = array[0];
+        array[0] = array[3];
+        array[3] = temp;
+
+        temp = array[1];
+        array[1] = array[2];
+        array[2] = temp;
+
+        temp = array[4];
+        array[4] = array[3];
+        array[3] = temp;
+
         for (int i : array) {
             System.out.println(i);
         }
