201ed25231dc104383c9ae87835fcbf733221e4c
Капустин Дмитрий <onewaykd@gmail.com>
2024-05-31 14:26:38 +0300

    6.1. Классический поиск перебором. [#33489 #34556]

--- src/test/java/ru/j4j/array/FindLoopTest.java
+++ src/test/java/ru/j4j/array/FindLoopTest.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Test;
 class FindLoopTest {
     @Test
     public void whenArrayHas5Then0() {
-        int[] data = new int[] {5, 10, 3};
+        int[] data = new int[]{5, 10, 3};
         int el = 5;
         int result = FindLoop.indexOf(data, el);
         int expected = 0;
@@ -16,7 +16,7 @@ class FindLoopTest {
 
     @Test
     public void whenDiapasonHas8ThenResultEqualFinish() {
-        int[] data = new int[] {5, 2, 10, 2, 4, 8, 14, 3, 21, 16};
+        int[] data = new int[]{5, 2, 10, 2, 4, 8, 14, 3, 21, 16};
         int el = 8;
         int start = 2;
         int finish = 5;
@@ -27,7 +27,7 @@ class FindLoopTest {
 
     @Test
     public void whenDiapasonHasSomeEqualElementThen3() {
-        int[] data = new int[] {5, 10, 2, 4, 8, 4, 14, 4, 3, 21, 16};
+        int[] data = new int[]{5, 10, 2, 4, 8, 4, 14, 4, 3, 21, 16};
         int el = 4;
         int start = 1;
         int finish = 8;
@@ -38,7 +38,7 @@ class FindLoopTest {
 
     @Test
     public void whenDiapasonHas2Then3() {
-        int[] data = new int[] {5, 2, 10, 2, 4};
+        int[] data = new int[]{5, 2, 10, 2, 4};
         int el = 2;
         int start = 2;
         int finish = 4;
@@ -49,12 +49,68 @@ class FindLoopTest {
 
     @Test
     public void whenDiapasonHasNot8ThenMinus1() {
-        int[] data = new int[] {8, 2, 10, 2, 4};
+        int[] data = new int[]{8, 2, 10, 2, 4};
         int el = 8;
         int start = 2;
         int finish = 4;
         int result = FindLoop.indexInRange(data, el, start, finish);
         int expected = -1;
         assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    public void whenElementFound_thenReturnIndex() {
+        int[] data = {5, 10, 3, 7};
+        int result = FindLoop.indexOf(data, 10);
+        assertThat(result).isEqualTo(1);
+    }
+
+    @Test
+    public void whenElementNotFound_thenReturnMinusOne() {
+        int[] data = {5, 10, 3, 7};
+        int result = FindLoop.indexOf(data, 8);
+        assertThat(result).isEqualTo(-1);
+    }
+
+    @Test
+    public void whenElementFoundAtStart_thenReturnIndex0() {
+        int[] data = {5, 10, 3, 7};
+        int result = FindLoop.indexOf(data, 5);
+        assertThat(result).isEqualTo(0);
+    }
+
+    @Test
+    public void whenElementFoundAtEnd_thenReturnLastIndex() {
+        int[] data = {5, 10, 3, 7};
+        int result = FindLoop.indexOf(data, 7);
+        assertThat(result).isEqualTo(3);
+    }
+
+    @Test
+    public void whenElementFoundInRange_thenReturnIndex() {
+        int[] data = {5, 10, 3, 7, 4};
+        int result = FindLoop.indexInRange(data, 3, 1, 3);
+        assertThat(result).isEqualTo(2);
+    }
+
+    @Test
+    public void whenElementNotFoundInRange_thenReturnMinusOne() {
+        int[] data = {5, 10, 3, 7, 4};
+        int result = FindLoop.indexInRange(data, 4, 1, 3);
+        assertThat(result).isEqualTo(-1);
+    }
+
+    @Test
+    public void whenElementFoundAtStartOfRange_thenReturnStartIndex() {
+        int[] data = {5, 10, 3, 7, 4};
+        int result = FindLoop.indexInRange(data, 10, 1, 4);
+        assertThat(result).isEqualTo(1);
+    }
+
+    @Test
+    public void whenElementFoundAtEndOfRange_thenReturnEndIndex() {
+        int[] data = {5, 10, 3, 7, 4};
+        int result = FindLoop.indexInRange(data, 4, 1, 4);
+        assertThat(result).isEqualTo(4);
     }
 }
