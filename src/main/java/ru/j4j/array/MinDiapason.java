4fdebcfdba03a2763e16f7b03bdfd746e7233c57
Капустин Дмитрий <onewaykd@gmail.com>
2024-05-31 15:51:21 +0300

    6.5.2. Поиск минимума в диапазоне. [#179804 #34563]

--- src/main/java/ru/j4j/array/MinDiapason.java
+++ src/main/java/ru/j4j/array/MinDiapason.java
@@ -3,7 +3,7 @@ package ru.j4j.array;
 public class MinDiapason {
     public static int findMinDiapason(int[] array, int start, int finish) {
         int min = array[start];
-        for (int i = start; i <= finish; i++) {
+        for (int i = start + 1; i <= finish; i++) {
             if (array[i] < min) {
                 min = array[i];
             }
