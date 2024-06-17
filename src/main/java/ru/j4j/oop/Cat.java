510c477b49dfe535564b64eebda3d4fb267b8f5c
Dmitrii <temzor@mail.ru>
2024-06-17 10:11:36 +0300

    1.1. Создание объекта [#165452 #34737]

---
+++ src/main/java/ru/j4j/oop/Cat.java
@@ -0,0 +1,8 @@
+package ru.j4j.oop;
+
+public class Cat {
+    public static void main(String[] args) {
+        Cat peppy = new Cat();
+        Cat sparky = new Cat();
+    }
+}
---
+++ src/main/java/ru/j4j/oop/Dog.java
@@ -0,0 +1,9 @@
+package ru.j4j.oop;
+
+public class Dog {
+    public static void main(String[] args) {
+        Dog tuzil = new Dog();
+        Dog sharik = new Dog();
+        Dog palkan = new Dog();
+    }
+}
