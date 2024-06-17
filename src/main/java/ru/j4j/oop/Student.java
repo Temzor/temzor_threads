afb78f2b8f3e44e5e25d58f0025661e9a5ef76f2
Dmitrii <temzor@mail.ru>
2024-06-17 10:42:28 +0300

    1.2. Вызов метода объекта [#166085 #34729]

---
+++ src/main/java/ru/j4j/oop/Student.java
@@ -0,0 +1,16 @@
+package ru.j4j.oop;
+
+public class Student {
+    public static void music() {
+        System.out.println("ta-ta-ta-ta-ta-ta-ta-ta-ta-ta");
+    }
+
+    public static void sing() {
+        System.out.println("I believe in Java");
+    }
+
+    public static void main(String[] args) {
+        Student.music();
+        Student.sing();
+    }
+}
