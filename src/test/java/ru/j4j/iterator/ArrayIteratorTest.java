9a633e09b8986d465fc625c447714b40982e6ebb
Капустин Дмитрий <onewaykd@gmail.com>
2024-06-20 14:08:26 +0300

    2. Подключение библиотеки AssertJ [#504881 #346339]

--- src/test/java/ru/j4j/iterator/ArrayIteratorTest.java
+++ src/test/java/ru/j4j/iterator/ArrayIteratorTest.java
@@ -1,37 +1,23 @@
 package ru.j4j.iterator;
 
-import static org.assertj.core.api.Assertions.*;
-
 import org.junit.jupiter.api.Test;
 
-import java.util.NoSuchElementException;
+import static org.assertj.core.api.Assertions.*;
 
-
-public class ArrayIteratorTest {
-
+class ArrayIteratorTest {
     @Test
-    public void whenMultiCallhasNextThenTrue() {
-        ArrayIterator iterator = new ArrayIterator(
-                new int[] {1, 2, 3}
-        );
+    void whenMultiCallHasNextThenTrue() {
+        ArrayIterator iterator = new ArrayIterator(new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9});
+        boolean result = iterator.hasNext();
+        assertThat(result).isTrue();
+        iterator.next();
         assertThat(iterator.hasNext()).isTrue();
     }
-
     @Test
-    public void whenReadSequence() {
-        ArrayIterator iterator = new ArrayIterator(
-                new int[] {1, 2, 3}
-        );
+    void whenReadSequence() {
+        ArrayIterator iterator = new ArrayIterator(new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9});
         assertThat(iterator.next()).isEqualTo(1);
         assertThat(iterator.next()).isEqualTo(2);
         assertThat(iterator.next()).isEqualTo(3);
-    }
-
-    @Test
-    public void whenNextFromEmpty() {
-        ArrayIterator iterator = new ArrayIterator(
-                new int[] {}
-        );
-        assertThatThrownBy(iterator::next).isInstanceOf(NoSuchElementException.class);
     }
 }
