cd8d5f9727471fe48931f6bfbb15a2d2b08eb71c
temzor <onewaykd@gmail.com>
2024-06-20 11:53:28 +0300

    BinarySearch

--- src/main/java/ru/algos/BinarySearch.java
+++ src/main/java/ru/algos/BinarySearch.java
@@ -18,7 +18,7 @@ public class BinarySearch {
         int high = arr.length - 1;
 
         while (low <= high) {
-            int mid = low + (high - low) / 2; // Avoid overflow
+            int mid = low + (high - low) / 2;
 
             if (arr[mid] == target) {
                 return mid;
